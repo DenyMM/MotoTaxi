80871c9dbc6d01ca20965aa9c87213e5875a8b0b
DenyMM <deny.lolipop@gmail.com>
2019-11-07 10:44:31 -0600

    Mapa agregado

--- MotoTaxi/src/app/pages/opcion/opcion.module.ts
+++ MotoTaxi/src/app/pages/opcion/opcion.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
+import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { OpcionPage } from './opcion.page';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: OpcionPage
-  }
-];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild([
+      {
+        path: '',
+        component: OpcionPage
+      }
+    ])
   ],
   declarations: [OpcionPage]
 })
